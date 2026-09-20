abc3283b71ac1fb1aae164721c6eeb23d9e8c2c3
lavesh6395 <lavesh6395@gmail.com>
2026-09-20 11:28:27 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+
+from app.routes.image import router as image_router
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -14,6 +16,7 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 templates = Jinja2Templates(directory="app/templates")
 
+app.include_router(image_router)
 
 @app.api_route("/", methods=["GET", "HEAD"], response_class=HTMLResponse)
 async def home(request: Request):

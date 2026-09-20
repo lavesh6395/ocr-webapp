aebd618c7006b290de2aad4f06b1eaeef5c5148a
lavesh6395 <lavesh6395@gmail.com>
2026-09-20 10:34:30 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
+from fastapi import FastAPI
 
+app = FastAPI(
+    title="OCR WebApp",
+    version="0.1.0",
+    description="Notebook-to-PDF AI"
+)
+
+@app.get("/")
+async def root():
+    return {
+        "message": "OCR WebApp is running.",
+        "version": "0.1.0"
+    }
